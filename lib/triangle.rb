05aa1a9f7443f6c5fb35f6693bf85a7132797768
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-16 18:57:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :equilateral, :isosceles, :scalene
+
+  def initialize(side_one, side_two, side_three)
+    @a = side_one
+    @b= side_two
+    @c = side_three
+
+  end
+
+  def kind
+    if (@a == 0 || @b == 0 || @c == 0) || (((@a + @b) < @c) || ((@a + @c) < @b) || ((@b + @c) < @a))
+      raise TriangleError
+
+  elsif @a == @b && @a == @c
+    @equilateral
+
+  elsif @a == @b || @a == @c || @b == @c
+      @isosceles
+  else
+    @scalene
+  end
+end
+
+class TriangleError < StandardError
+
+def message
+  "Not a real Triangle"
+end
+
+end
 end
