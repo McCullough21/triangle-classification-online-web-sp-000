2abcbe1233e0b54c6d5cda1080685e07998a6200
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-16 19:03:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,20 @@
 # class Triangle
 #   attr_accessor :a, :b, :c, :equilateral, :isosceles, :scalene
-# 
+#
 #   def initialize(side_one, side_two, side_three)
 #     @a = side_one
 #     @b= side_two
 #     @c = side_three
 #   end
-# 
+#
 # def equ
 #   def kind
 #     if (@a == 0 || @b == 0 || @c == 0) || (((@a + @b) < @c) || ((@a + @c) < @b) || ((@b + @c) < @a))
 #       raise TriangleError
-# 
+#
 #   elsif @a == @b && @a == @c
 #     @equilateral
-# 
+#
 #   elsif @a == @b || @a == @c || @b == @c
 #       @isosceles
 #   else
